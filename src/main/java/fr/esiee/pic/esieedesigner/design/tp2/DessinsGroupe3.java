1fe7680d5ece90fd53214b0b3901e4876a1a0b00
i.domun <irfaan.domun@edu.esiee.fr>
2016-11-21 19:02:12 +0100

    add the body and hands

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
@@ -52,7 +52,8 @@ public class DessinsGroupe3 extends CreateurDeForme {
 		.ajouter(hautDroit)
 		.ajouter(basDroite)
 		.ajouter(basGauche);
-
+		
+		dessinerCorp();
 	
 	}
 	
@@ -60,34 +61,83 @@ public class DessinsGroupe3 extends CreateurDeForme {
 	 * Methode pour dessiner le corp et les doides
 	 */
 	public void dessinerCorp() {
-		Point p1 = new Point(1,4);/*
-		Point p2 = new Point(1,4);
-		Point p3 = new Point();
-		Point p4 = new Point();
-		Point p5 = new Point();
-		Point p6 = new Point();
-		Point p7 = new Point();
-		Point p8 = new Point();
-		Point p9 = new Point();
-		Point p10 = new Point();
-		Point p11 = new Point();
-		Point p12 = new Point();
-		Point p13 = new Point();
-		Point p14 = new Point();
-		Point p15 = new Point();
-		Point p16 = new Point();
-		Point p17 = new Point();
-		Point p18 = new Point();
-		Point p19 = new Point();
-		Point p20 = new Point();
-		Point p21 = new Point();
-		Point p22 = new Point();
-		Point p23 = new Point();
-		Point p24 = new Point();
-		Point p25 = new Point();
-		Point p26 = new Point();
-		Point p27 = new Point();
-		Point p28 = new Point();*/
+		// Pour le corp
+		Point p1 = new Point(BASE_GAUCHE+6*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p2 = new Point(BASE_GAUCHE+8*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p3 = new Point(BASE_GAUCHE+10*PIXEL,BASE_HAUT+8*PIXEL);
+		Point p4 = new Point(BASE_GAUCHE+11*PIXEL,BASE_HAUT+8*PIXEL);
+		Point p5 = new Point(BASE_GAUCHE+13*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p6 = new Point(BASE_GAUCHE+13*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p7 = new Point(BASE_GAUCHE+12*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p8 = new Point(BASE_GAUCHE+12*PIXEL,BASE_HAUT+11*PIXEL);
+		Point p9 = new Point(BASE_GAUCHE+11*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p10 = new Point(BASE_GAUCHE+9*PIXEL,BASE_HAUT+11*PIXEL);
+		Point p11 = new Point(BASE_GAUCHE+10*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p12 = new Point(BASE_GAUCHE+11*PIXEL,BASE_HAUT+14*PIXEL);
+		Point p13 = new Point(BASE_GAUCHE+8*PIXEL,BASE_HAUT+14*PIXEL);
+		Point p14 = new Point(BASE_GAUCHE+9*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p15 = new Point(BASE_GAUCHE+8*PIXEL,BASE_HAUT+12*PIXEL);
+		Point p16 = new Point(BASE_GAUCHE+6*PIXEL,BASE_HAUT+12*PIXEL);
+		Point p17 = new Point(BASE_GAUCHE+5*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p18 = new Point(BASE_GAUCHE+6*PIXEL,BASE_HAUT+14*PIXEL);
+		Point p19 = new Point(BASE_GAUCHE+3*PIXEL,BASE_HAUT+14*PIXEL);
+		Point p20 = new Point(BASE_GAUCHE+4*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p21 = new Point(BASE_GAUCHE+5*PIXEL,BASE_HAUT+11*PIXEL);
+		Point p22 = new Point(BASE_GAUCHE+3*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p23 = new Point(BASE_GAUCHE+2*PIXEL,BASE_HAUT+11*PIXEL);
+		Point p24 = new Point(BASE_GAUCHE+2*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p25 = new Point(BASE_GAUCHE+1*PIXEL,BASE_HAUT+13*PIXEL);
+		Point p26 = new Point(BASE_GAUCHE+1*PIXEL,BASE_HAUT+10*PIXEL);
+		Point p27 = new Point(BASE_GAUCHE+3*PIXEL,BASE_HAUT+8*PIXEL);
+		Point p28 = new Point(BASE_GAUCHE+4*PIXEL,BASE_HAUT+8*PIXEL);
+		
+		// Pour les mains
+		Point pA = new Point(BASE_GAUCHE+12*PIXEL,BASE_HAUT+12*PIXEL);
+		Point pB = new Point(BASE_GAUCHE+13*PIXEL,BASE_HAUT+12*PIXEL);
+		Point pC = new Point(BASE_GAUCHE+1*PIXEL,BASE_HAUT+12*PIXEL);
+		Point pD = new Point(BASE_GAUCHE+2*PIXEL,BASE_HAUT+12*PIXEL);
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(p1)
+		.ajouter(p2)
+		.ajouter(p3)
+		.ajouter(p4)
+		.ajouter(p5)
+		.ajouter(pB)
+		.ajouter(pA)
+		.ajouter(p8)
+		.ajouter(p9)
+		.ajouter(p10)
+		.ajouter(p11)
+		.ajouter(p12)
+		.ajouter(p13)
+		.ajouter(p14)
+		.ajouter(p15)
+		.ajouter(p16)
+		.ajouter(p17)
+		.ajouter(p18)
+		.ajouter(p19)
+		.ajouter(p20)
+		.ajouter(p21)
+		.ajouter(p22)
+		.ajouter(p23)
+		.ajouter(pD)
+		.ajouter(pC)
+		.ajouter(p26)
+		.ajouter(p27)
+		.ajouter(p28);
+
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(pA)
+		.ajouter(pB)
+		.ajouter(p6)
+		.ajouter(p7);
+
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(pC)
+		.ajouter(pD)
+		.ajouter(p24)
+		.ajouter(p25);
 	}
 
 }
