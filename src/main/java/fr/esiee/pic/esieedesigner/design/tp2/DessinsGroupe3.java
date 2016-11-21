dec95641f817c59e4fc3282409b753510aee1ede
dr.chaucour <dr.chaucour@gmail.com>
2016-11-21 18:04:50 +0100

    add base points

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
@@ -1,5 +1,6 @@
 package fr.esiee.pic.esieedesigner.design.tp2;
 
+import fr.esiee.pic.esieedesigner.api.shapes.Point;
 import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 
 /**
@@ -8,13 +9,50 @@ import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
  * @author etudiant
  *
  */
+
+
 public class DessinsGroupe3 extends CreateurDeForme {
+	
+	private static final int PIXEL = 25;
+	
+	private static final int LONGUEUR_PAGE = 77 * PIXEL;
+	
+	private static final int LARGEUR_PAGE = 41 * PIXEL;
+	
+	private static final int LARGEUR_PERSO = 14 * PIXEL;
+	
+	private static final int LONGUEUR_PERSO = 14 * PIXEL;
+
+	private static final int BASE_HAUT = LARGEUR_PAGE/2-LARGEUR_PERSO/2;
+	
+	private static final int BASE_BAS = LARGEUR_PAGE/2 + LARGEUR_PERSO/2;
+	
+	private static final int BASE_DROITE = LONGUEUR_PAGE/2 + LONGUEUR_PERSO/2;
+	
+	private static final int BASE_GAUCHE = LONGUEUR_PAGE/2 - LONGUEUR_PERSO/2;
+
+	
+	private static final Point BASE_NEZ_HAUT = new Point(950, 750);
+
 
 	@Override
 	public void dessiner() {
 		// Ajout vos dessins ici
 		// Pensez à désactiver les dessins de la démo 
 		// en commentant initDemo dans le méthode init()de la classe fr.esiee.pic.esieedesigner.ui.scene.EcranPrincipal
+		
+		Point hautGauche = new Point(BASE_GAUCHE, BASE_HAUT);
+		Point hautDroit = new Point(BASE_DROITE, BASE_HAUT);
+		Point basGauche = new Point(BASE_GAUCHE, BASE_BAS);
+		Point basDroite = new Point(BASE_DROITE, BASE_BAS);
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(hautGauche)
+		.ajouter(hautDroit)
+		.ajouter(basDroite)
+		.ajouter(basGauche);
+
+
 	}
 
 }
